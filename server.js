311f94f1cbc29ab1e35ef4adad34ff2b9664255b
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-09-13 15:11:29 -0500

    built mongoose schema and seed the database

--- server.js
+++ server.js
@@ -3,9 +3,29 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+app.use(cors());
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/test');
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('connected to mongo!');
+});
+
+// import our Mongoose model
+const Book = require('./models/book');
 
 const app = express();
-app.use(cors());
+
+// Route handlers
+app.get('/books', async (req, res) => {
+  const books = await Book.find();
+
+  res.send(books);
+})
 
 const PORT = process.env.PORT || 3001;
 
