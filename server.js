50f6905e5afa8d855404770e88d767e0e7a40368
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-09-16 14:15:55 -0500

    fixed syntax error

--- server.js
+++ server.js
@@ -3,15 +3,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect(process.env.MONGODB_URL);
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function(){
+db.once('open', function() {
   console.log('connected to mongo!');
 });
 
@@ -22,19 +21,25 @@ const { response } = require('express');
 const app = express();
 app.use(cors()); 
 
+app.use(express.json());
+
 // Route handlers
 app.get('/books', (req, res) => {
     Book.find((err, bookResponse) => {
     console.log(bookResponse);
     res.send(bookResponse);
+
   });
   
 })
 
 app.post('/books', postBooks);
+app.delete('/books/:id', deleteBook);
+app.put('/books/:id', putBook);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3010;
 
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 async function postBooks(req, res) {
   console.log('headers', req.headers);
@@ -45,7 +50,8 @@ async function postBooks(req, res) {
 }
 
 async function deleteBook(req, res) {
-  let id = req.params.id
+  console.log('this is my request', req);
+  let id = req.params.id;
 
   try {
     await Book.findByIdAndDelete(id);
@@ -58,6 +64,7 @@ async function deleteBook(req, res) {
 
 function handleError(err, res) {
   console.error(err);
+  res.status(500).send('oops!');
 }
 
 app.get('/test', (request, response) => {
@@ -66,4 +73,22 @@ app.get('/test', (request, response) => {
 
 })
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+async function putBook(req, res) {
+  let id = req.params.id;
+  let bookUpdate = req.body;
+
+  let options = {
+    new: true,
+    overwrite: true,
+  }
+
+  try {
+    let updatedBook = await Book.findByIdAndUpdate(id, bookUpdate, options)
+    res.send(updatedBook);
+
+  } catch (err) {
+    handleError(err, res);
+  }
+}
+
+
